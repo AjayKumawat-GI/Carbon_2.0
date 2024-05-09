28b956ac326abb4cf1e2d880e066b3a3c3932b47
AjayKumawat-GI <ajay.kumawat@growindigo.co.in>
2024-05-09 14:43:35 +0530

    Register Api mediator dependency in dependency container

---
+++ Carbon/Infrastructure/Installers/ApiMediatorInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Referral.API.Infrastructure.Installers;
+using Referral.Mediator.Infrastructure;
+
+namespace Carbon.API.Infrastructure.Installers
+{
+    public class ApiMediatorInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                //ApiClients
+                .AddTransient(typeof(IApiClient), typeof(ApiClient));
+        }
+    }
+}
