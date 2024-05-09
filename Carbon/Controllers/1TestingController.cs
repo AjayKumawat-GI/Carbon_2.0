19b1854b5d73d0ee6b3fbd8fe7eb7fea18f3e3a2
AjayKumawat-GI <ajay.kumawat@growindigo.co.in>
2024-05-09 14:53:40 +0530

    Testing an open API

--- Carbon/Controllers/1TestingController.cs
+++ Carbon/Controllers/1TestingController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Referral.Mediator.Infrastructure;
 
 namespace Carbon.API.Controllers
 {
@@ -7,10 +8,24 @@ namespace Carbon.API.Controllers
     [ApiController]
     public class _1TestingController : ControllerBase
     {
+        private readonly IApiClient _apiClient;
+        public _1TestingController(IApiClient apiClient)
+        {
+                _apiClient = apiClient;
+        }
+
+        [HttpPost]
+        [Route("TestingAPIMediator")]
+        public void Get()
+        {
+            _apiClient.CallApiAsync("https://webhook.site/c5f88d44-4c75-41cf-843f-0dbacb9c964d", HttpMethod.Post, "");
+        }
+
         [HttpGet(Name = "ErrorTesting")]
         public void GetErrorTesting()
         {
             throw new NotImplementedException("Something went wrong");
         }
+
     }
 }
